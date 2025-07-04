4c9fc675a7bc6163bd1dce597a8f121b8f86f633
Nessan本Ne <randoservice@proton.me>
2025-07-04 22:56:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -177,7 +177,7 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/rulelist ]]; then
         cp rulelist /etc/XrayR/
     fi
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/RandomNessan/XrayR-release/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/RandomNessan/xrayr_install_oneclick/main/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
